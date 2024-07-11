32d89fc69b88207501a65148b46e877f732ad9b8
Daniel Cavalcante <dvictor.cintra@gmail.com>
2024-07-11 15:49:14 -0300

    refactor: add PlayerHeader to ExercisePlayer

--- frontend/src/pages/Exercicio/ExerciseVideoPage.jsx
+++ frontend/src/pages/Exercicio/ExerciseVideoPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { useParams } from 'react-router-dom';
 
-import Player from '../../components/Player';
+import Player from '../../components/exercise/exercisePlayer/ExercisePlayer';
 
 // const videoData = {
 //   pulso: {
