3fd1a4de47e054ff87c8879d6b4dc8b4bb0c3f3f
Daniel Cavalcante <dvictor.cintra@gmail.com>
2024-07-14 19:17:24 -0300

    feat: add extra step in modal window for feeling pain

--- frontend/src/components/home/ModalPainContent.jsx
+++ frontend/src/components/home/ModalPainContent.jsx
@@ -1,41 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ModalPainContent.module.css";
 
+import ModalLetsMakeItBetter from "./ModalLetsMakeItBetter";
 import pulso from "../../assets/images/pulso.png";
 import lombar from "../../assets/images/lombar.png";
 import perna from "../../assets/images/perna.png";
 import pescoco from "../../assets/images/pescoco.png";
 
 const ModalPainContent = () => {
+  const [step, setStep] = useState(1);
+
+  const handleButtonClick = () => {
+    setStep(2);
+  };
+
   return (
     <div>
-      <h2>Onde?</h2>
-      <div className={styles.buttons}>
+      {step === 1 ? (
         <div>
-          <p>Pulso</p>
-          <button className={styles.areaButton}>
-            <img src={pulso} alt="pulso" className={styles.icon} />
-          </button>
+          <h2>Onde?</h2>
+          <div className={styles.buttons}>
+            <div>
+              <p>Pulso</p>
+              <button className={styles.areaButton} onClick={handleButtonClick}>
+                <img src={pulso} alt="pulso" className={styles.icon} />
+              </button>
+            </div>
+            <div>
+              <p>Lombar</p>
+              <button className={styles.areaButton} onClick={handleButtonClick}>
+                <img src={lombar} alt="lombar" className={styles.icon} />
+              </button>
+            </div>
+            <div>
+              <p>Perna</p>
+              <button className={styles.areaButton} onClick={handleButtonClick}>
+                <img src={perna} alt="perna" className={styles.icon} />
+              </button>
+            </div>
+            <div>
+              <p>Pescoço</p>
+              <button className={styles.areaButton} onClick={handleButtonClick}>
+                <img src={pescoco} alt="pescoço" className={styles.icon} />
+              </button>
+            </div>
+          </div>
         </div>
-        <div>
-          <p>Lombar</p>
-          <button className={styles.areaButton}>
-            <img src={lombar} alt="lombar" className={styles.icon} />
-          </button>
-        </div>
-        <div>
-          <p>Perna</p>
-          <button className={styles.areaButton}>
-            <img src={perna} alt="perna" className={styles.icon} />
-          </button>
-        </div>
-        <div>
-          <p>Pescoço</p>
-          <button className={styles.areaButton}>
-            <img src={pescoco} alt="pescoço" className={styles.icon} />
-          </button>
-        </div>
-      </div>
+      ) : (
+        <ModalLetsMakeItBetter />
+      )}
     </div>
   );
 };
