ea550ec3d4fadfeb1cc22887d15404b48433377b
Daniel Cavalcante <dvictor.cintra@gmail.com>
2024-08-14 00:16:00 -0300

    feat: create form Progress Bar

--- frontend/src/components/form/FormHeader.jsx
+++ frontend/src/components/form/FormHeader.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import styles from "./FormHeader.module.css";
 
-const FormHeader = ({ answers }) => {
+import ProgressBar from "./ProgressBar";
+
+const FormHeader = ({ step }) => {
   return (
     <div className={styles.formHeaderContainer}>
       <div className={styles.formHeader}>
-        <h3>{`Perguntas ${answers} de 8`}</h3>
-        <h3>{`${(answers / 8) * 100}%`}</h3>
+        <h3>{`Perguntas ${step} de 8`}</h3>
+        <div className={styles.progress}>
+          <h3>{`${(step / 8) * 100}%`}</h3>
+          <ProgressBar state={step} />
+        </div>
       </div>
     </div>
   );
